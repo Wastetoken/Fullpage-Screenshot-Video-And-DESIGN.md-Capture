b22253a0121be361db57fd463047d258891194db
Wastetoken <wastetoken47@gmail.com>
2026-09-03 20:09:58 -0700

    Add files via upload

---
+++ Visual-Capture/assets/init-C7CqfD1Z.js
@@ -0,0 +1,1 @@
+const o=navigator.userAgent,t=o.includes("Chrome")?chrome:browser;let e="https://cocoshot.net";export{e as H,t as W};
---
+++ Visual-Capture/assets/offscreen-C4n06ih5.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-B32Nu7ID.js";navigator.serviceWorker.onmessage=e=>{e.ports[0].postMessage(URL.createObjectURL(e.data))};
